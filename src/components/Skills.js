05c016a61f6d51ebb3cd613149968b976c5a6cf4
Michael Antoni <michael@bluehive.com.ph>
2020-05-05 22:43:15 +0800

    links to skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,50 +10,42 @@ class Skill extends Component {
 	              <h2 className="mb-5">Skills</h2>
 
 	              <div className="subheading mb-3">Programming Languages &amp; Tools</div>
-	              <ul className="list-inline dev-icons">
+	              <ul className="list-inline dev-icons"> 
 	              	<li className="list-inline-item">
-	                  	<i className="devicon-bootstrap-plain colored fa-2x"></i>
-	                </li>	 
-	              	<li className="list-inline-item">
-	                  	<i className="devicon-javascript-plain colored fa-2x"></i>
+	                  	<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" className="text-secondary" target="_blank"><i className="devicon-javascript-plain colored fa-2x"></i></a>
 	                </li>	               	   
 	                <li className="list-inline-item">
-	                  	<i className="devicon-jquery-plain-wordmark colored fa-2x"></i>
+	                  	<a href="https://jquery.com/" className="text-secondary" target="_blank"><i className="devicon-jquery-plain-wordmark colored fa-2x"></i></a>
 	                </li>	
 	                <li className="list-inline-item">
-	                  	<i className="devicon-php-plain fa-2x"></i>
+	                  	<a href="https://www.php.net/manual/en/" className="text-secondary" target="_blank"><i className="devicon-php-plain fa-2x"></i></a>
 	                </li>
 	               	<li className="list-inline-item">
-	                  	<i className="devicon-laravel-plain colored fa-2x"></i>
+	                  	<a href="https://laravel.com/" className="text-secondary" target="_blank"><i className="devicon-laravel-plain colored fa-2x"></i></a>
 	                </li>
 	               	<li className="list-inline-item">
-	                  	<i className="devicon-react-original colored fa-2x"></i>
+	                  	<a href="https://reactjs.org/" className="text-secondary" target="_blank"><i className="devicon-react-original colored fa-2x"></i></a>
 	                </li>
 	                <li className="list-inline-item">
-	                  	<i className="devicon-nodejs-plain colored fa-2x"></i>
+	                  	<a href="https://nodejs.org/" className="text-secondary" target="_blank"><i className="devicon-nodejs-plain colored fa-2x"></i></a>
 	                </li>
 
 	                <li className="list-inline-item">
-	                 	<i className="devicon-mysql-plain-wordmark colored fa-2x"></i>
+	                 	<a href="https://www.mysql.com/" className="text-secondary" target="_blank"><i className="devicon-mysql-plain-wordmark colored fa-2x"></i></a>
+	                </li>
+
+	                <li className="list-inline-item">
+	                	<a href="https://www.mongodb.com/" className="text-secondary" target="_blank"><i className="devicon-mongodb-plain-wordmark colored fa-2x"></i></a>
 	                </li>
 	                <li className="list-inline-item">
-	                  	<i className="devicon-postgresql-plain colored fa-2x"></i>
+	                  	<a href="https://git-scm.com/" className="text-secondary" target="_blank"><i className="devicon-git-plain colored fa-2x"></i></a>
 	                </li>
 	                <li className="list-inline-item">
-	                	<i className="devicon-mongodb-plain-wordmark colored fa-2x"></i>
-	                </li>
-	                <li className="list-inline-item">
-	                  	<i className="devicon-git-plain colored fa-2x"></i>
-	                </li>
-	                <li className="list-inline-item">
-	                  	<i className="devicon-bitbucket-plain colored fa-2x"></i>
+	                  	<a href="https://bitbucket.org/" className="text-secondary" target="_blank"><i className="devicon-bitbucket-plain colored fa-2x"></i></a>
 	                </li>	    
 	                <li className="list-inline-item">
-	                  	<i className="devicon-ubuntu-plain colored fa-2x"></i>
-	                </li>
-	               	<li className="list-inline-item">
-	                  	<i className="devicon-photoshop-plain colored fa-2x"></i>
-	                </li>		                	                             			
+	                  	<a href="https://ubuntu.com/" className="text-secondary" target="_blank"><i className="devicon-ubuntu-plain colored fa-2x"></i></a>
+	                </li>	                	                             			
 	              </ul>
 	            </div>
 	          </section>
