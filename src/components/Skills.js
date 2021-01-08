9dfb1a14398721c5b96b7b202b7d4b5a96a13e95
Michael Antoni <michaelantoni.cs@gmail.com>
2021-01-08 23:00:30 +0800

    update svg icons

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,84 +1,43 @@
 import React from 'react';
-import 'devicon/devicon.css';
 
 function Skill () {
+
+	const iconsData = [
+		{ url: 'https://www.adobe.com/sea/products/photoshop.html', svg: 'ps.svg'},
+		{ url: 'https://code.visualstudio.com/', svg: 'vscode.svg' },
+		{ url: 'https://www.w3schools.com/html/default.asp', svg: 'html.svg' },
+		{ url: 'https://www.w3schools.com/css/default.asp', svg: 'css.svg' },
+		{ url: 'https://www.w3schools.com/js/DEFAULT.asp', svg: 'js.svg' },
+		{ url: 'https://jquery.com/', svg: 'jq.svg' },
+		{ url: 'https://getbootstrap.com/', svg: 'bs.svg' },
+		{ url: 'https://www.php.net/', svg: 'php.svg' },
+		{ url: 'https://www.mysql.com/', svg: 'mysql.svg' },
+		{ url: 'https://firebase.google.com/', svg: 'firebase.svg' },
+		{ url: 'https://www.mongodb.com/', svg: 'mongodb.svg' },
+		{ url: 'https://reactjs.org/', svg: 'react.svg' },
+		{ url: 'https://redux.js.org/', svg: 'redux.svg' },
+		{ url: 'https://www.npmjs.com/', svg: 'npm.svg' },
+		{ url: 'https://nodejs.org/en/', svg: 'node.svg' },
+		{ url: 'https://laravel.com/', svg: 'laravel.svg' },
+		{ url: 'https://lumen.laravel.com/', svg: 'lumen.svg' },
+		{ url: 'https://ubuntu.com/', svg: 'ubuntu.svg' },
+		{ url: 'https://www.microsoft.com/en-ph/windows', svg: 'windows.svg' },
+		{ url: 'https://git-scm.com/', svg: 'git.svg'},
+		{ url: 'https://github.com/', svg: 'github.svg'},
+		{ url: 'https://bitbucket.org/', svg: 'bb.svg'},
+	]
+
 	return (
 		<section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="skills">
 			<div className="w-100">
 				<h2 className="mb-5 wow slideInLeft text-secondary">Skills</h2>
-
-				<ul class="list-inline wow fadeInDown">
-					<li class="list-inline-item"><img src="assets/img/svg/photoshop.svg" height="60"/></li>
-					<li class="list-inline-item"><img src="assets/img/svg/html.svg" height="60"/></li>
-					<li class="list-inline-item"><img src="assets/img/svg/css3.svg" height="60"/></li>
-					<li class="list-inline-item"><img src="assets/img/svg/javascript.svg" height="80"/></li>
-					<li class="list-inline-item"><img className="mr-1" src="assets/img/svg/nodejs2.svg" height="60"/></li>
-					<li class="list-inline-item"><img className="mr-1" src="assets/img/svg/php.svg" height="50"/></li>
-					<li class="list-inline-item"><img src="assets/img/svg/mysql.svg" height="50"/></li>
-					<li class="list-inline-item"><img src="assets/img/svg/firebase.svg" height="80"/></li>
-					<li class="list-inline-item"><img src="assets/img/svg/mongodb2.svg" height="80"/></li>
-				</ul>
-
-				<ul className="list-inline mt-2 wow fadeInUp">
-					<li class="list-inline-item"><img src="assets/img/svg/react.svg" height="80"/></li>
-					<li class="list-inline-item"><img src="assets/img/svg/redux.svg" height="80"/></li>
-					<li class="list-inline-item"><img className="mr-2" src="assets/img/svg/npm.svg" height="30"/></li>
-					<li class="list-inline-item"><img src="assets/img/svg/laravel2.svg" height="60"/></li>
-					<li class="list-inline-item"><img className="mx-2" src="assets/img/svg/lumen.svg" height="60"/></li>
-					<li class="list-inline-item"><img src="assets/img/svg/ubuntu.svg" height="60"/></li>
-					<li class="list-inline-item"><img src="assets/img/svg/linux.svg" height="60"/></li>
-					<li class="list-inline-item"><img src="assets/img/svg/windows.svg" height="60"/></li>
-					<li class="list-inline-item"><img src="assets/img/svg/git.svg" height="80"/></li>
-					<li class="list-inline-item"><img src="assets/img/svg/bitbucket.png" height="60"/></li>
-				</ul>
-
-				{/* <ul className="list-inline dev-icons wow fadeInUp">
-					<li className="list-inline-item p-md-2">
-						<a href="https://www.photoshop.com/en" className="text-secondary shake-slow" target="_blank"><i class="devicon-photoshop-line colored fa-2x"></i></a>
-					</li>
-					<li className="list-inline-item p-md-2">
-						<a href="https://www.w3schools.com/html/default.asp" className="text-secondary shake-slow" target="_blank"><i class="devicon-html5-plain colored fa-2x"></i></a>
-					</li>
-					<li className="list-inline-item p-md-2">
-						<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/Reference" className="text-secondary shake-slow" target="_blank"><i class="devicon-css3-plain-wordmark colored fa-2x"></i></a>
-					</li>
-					<li className="list-inline-item p-md-2">
-						<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" className="text-secondary shake-slow" target="_blank"><i className="devicon-javascript-plain colored fa-2x"></i></a>
-					</li>
-					<li className="list-inline-item p-md-2">
-						<a href="https://jquery.com/" className="text-secondary shake-slow" target="_blank"><i className="devicon-jquery-plain-wordmark colored fa-2x"></i></a>
-					</li>
-					<li className="list-inline-item p-md-2">
-						<a href="https://git-scm.com/" className="text-secondary shake-slow" target="_blank"><i class="devicon-git-plain colored fa-2x"></i></a>
-					</li>
-					<li className="list-inline-item p-md-2">
-						<a href="https://github.com/" className="text-secondary shake-slow" target="_blank"><i class="devicon-github-plain-wordmark colored fa-2x"></i></a>
-					</li>
-
-					<li className="list-inline-item p-md-2">
-						<a href="https://www.php.net/manual/en/" className="text-secondary shake-slow" target="_blank"><i className="devicon-php-plain fa-2x"></i></a>
-					</li>
-					<li className="list-inline-item p-md-2">
-						<a href="https://laravel.com/" className="text-secondar shake-slow" target="_blank"><i className="devicon-laravel-plain-wordmark fa-2x text-secondary"></i></a>
-					</li>
-					<li className="list-inline-item p-md-2">
-						<a href="https://reactjs.org/" className="text-secondary shake-slow" target="_blank"><i className="devicon-react-original-wordmark colored fa-2x"></i></a>
-					</li>
-					<li className="list-inline-item p-md-2">
-						<a href="https://nodejs.org/" className="text-secondary shake-slow" target="_blank"><i className="devicon-nodejs-plain colored fa-2x"></i></a>
-					</li>
-
-					<li className="list-inline-item p-md-2">
-						<a href="https://www.mysql.com/" className="text-secondary shake-slow" target="_blank"><i className="devicon-mysql-plain-wordmark colored fa-2x"></i></a>
-					</li>
-
-					<li className="list-inline-item p-md-2">
-						<a href="https://www.mongodb.com/" className="text-secondary shake-slow" target="_blank"><i className="devicon-mongodb-plain-wordmark colored fa-2x"></i></a>
-					</li>
-					<li className="list-inline-item p-md-2">
-						<a href="https://ubuntu.com/" className="text-secondary shake-slow" target="_blank"><i className="devicon-linux-plain fa-2x"></i></a>
-					</li>
-				</ul> */}
+				<div className="skill-container">
+					{
+						iconsData.map(icon => (
+							<a href={icon.url} target="_blank"><img className="shake-slow skill-img" src={"assets/img/svg/" + icon.svg}/></a>
+						))
+					}
+				</div>
 			</div>
 		</section>
 	)
