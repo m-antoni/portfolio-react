d8cc666602f0e519be8cf1418a033eabeda1a0b2
Michael Antoni <michaelantoni.cs@gmail.com>
2021-10-02 15:12:51 +0800

    update expi

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -9,7 +9,7 @@ function Experience () {
 				<div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
 					<div className="resume-content wow fadeInUp">
 						<h3 className="mb-0">React JS Developer</h3>
-						<div className="subheading mb-3"><a className="text-primary" href="https://www.accenture.com/ph-en" target="_blank"><small><i className="fa fa-link"></i></small> Accenture</a></div>
+						<div className="subheading mb-3"><a className="text-primary" href="https://www.accenture.com/ph-en" target="_blank"> Accenture</a></div>
 						<ul>
 							<li>Designing and implementing ReactJS based applications Analyzing user requirements to inform application design Defining application objectives and functionality</li>
 							<li>Aligning application design with business goals</li>
@@ -24,11 +24,12 @@ function Experience () {
 				<div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
 					<div className="resume-content wow fadeInUp">
 						<h3 className="mb-0">Full Stack Web Developer</h3>
-						<div className="subheading mb-3"><a className="text-primary" href="https://bluehive.com.ph/" target="_blank"><small><i className="fa fa-link"></i></small> Bluehive</a></div>
+						<div className="subheading mb-3"><a className="text-primary" href="https://bluehive.com.ph/" target="_blank"> Bluehive</a></div>
 						<ul>
 							<li>Full-Stack Engineering</li>
 							<li>Create Reliable and Maintainable applications</li>
 							<li>Follow best practices in software development</li>
+							<li>Performed Agile Methodology (Scrum)</li>
 						</ul>
 					</div>
 					<div className="resume-date text-md-right">
@@ -39,10 +40,11 @@ function Experience () {
 				<div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
 					<div className="resume-content wow fadeInUp">
 						<h3 className="mb-0">Web Developer</h3>
-						<div className="subheading mb-3"><a className="text-primary" href="https://www.facebook.com/dcgroupintl" target="_blank"><small><i className="fa fa-link"></i></small> DC Multinational Group of Companies</a></div>
+						<div className="subheading mb-3"><a className="text-primary" href="https://www.facebook.com/dcgroupintl" target="_blank">DC Multinational Group of Companies</a></div>
 						<ul>
-							<li>Back-end Engineering (Laravel)</li>
-							<li>Adding new features and functionality</li>
+							<li>Front-end to Back-end Engineering</li>
+							<li>Add features and functionality to an existing code base</li>
+							<li>Performed Responsive Web Design</li>
 						</ul>
 					</div>
 					<div className="resume-date text-md-right">
