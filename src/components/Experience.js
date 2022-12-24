4d6bab29d9ea95f5b4384697dfbfd41bfffd3126
Michael Antoni <michaelantoni.cs@gmail.com>
2022-12-24 09:43:20 +0800

    update xp

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,7 +8,7 @@ function Experience () {
 
 				<div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
 					<div className="resume-content wow fadeInUp">
-						<h3 className="mb-0">Senior Software Engineer</h3>
+						<h3 className="mb-0">Advanced App Engineering Senior Analyst</h3>
 						<div className="subheading mb-3"><a className="text-primary" href="https://www.accenture.com/ph-en" target="_blank"> Accenture</a></div>
 					</div>
 					<div className="resume-date text-md-right">
@@ -18,7 +18,7 @@ function Experience () {
 
 				<div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
 					<div className="resume-content wow fadeInUp">
-						<h3 className="mb-0">Freelance: Full Stack Web Developer</h3>
+						<h3 className="mb-0">Freelance: MERN Stack Developer</h3>
 						<div>Project Based only</div>
 						<div className="subheading mb-3">Stealth Startup</div>
 					</div>
@@ -50,20 +50,10 @@ function Experience () {
 				<div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
 					<div className="resume-content wow fadeInUp">
 						<h3 className="mb-0">Computer Technician</h3>
-						<div className="subheading mb-3">Computer World</div>
+						<div className="subheading mb-3">Glenmhore Computer World</div>
 					</div>
 					<div className="resume-date text-md-right">
 						<span className="text-primary">Jul 2014 - Mar 2016</span>
-					</div>
-				</div>
-
-				<div className="resume-item d-flex flex-column flex-md-row justify-content-between">
-					<div className="resume-content wow fadeInUp">
-						<h3 className="mb-0">Technical Staff</h3>
-						<div className="subheading mb-3">Monster PrintAds</div>
-					</div>
-					<div className="resume-date text-md-right">
-						<span className="text-primary">Mar 2012 - Jun 2014</span>
 					</div>
 				</div>
 			</div>
